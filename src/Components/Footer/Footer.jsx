39acc40dac22f8dc9246fde48bb2aeab232e924d
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2024-02-21 19:54:56 +0500

    Adding Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
+import { Wrapper } from "../";
+
+import "./Styles.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Wrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="socialIcons">
+          <span className="icon">
+            <a target="_blank" href="https://www.facebook.com/mohammadyaseen.developer/"><FaFacebookF color="white" /></a>
+          </span>
+          <span className="icon">
+            <a target="_blank" href="https://www.instagram.com/_yaxeen_098_/"><FaInstagram color="white" /></a>
+          </span>
+          <span className="icon">
+            <a target="_blank" href="https://twitter.com/MohammadYa50019"> <FaTwitter color="white" /></a>
+          </span>
+          <span className="icon">
+            <a target="_blank" href="https://github.com/Mohammad-Yaseen69"><FaGithub color="white" /></a>
+          </span>
+        </div>
+      </Wrapper>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
